8f289858c033683c951553bb2aa96eaef34161b9
Ersapta Aristo <aristoersapta@gmail.com>
2025-08-31 23:15:24 +0700

    feat(icons): add new icons and update existing ones to use currentColor
    add icon-show-password, icon-selected, icon-bill-paid, icon-bill-due, icon-ellipsis, icon-close-modal, icon-hide-password, icon-recurring-bills, icon-pot
    update icon-caret-down, icon-caret-left, icon-caret-right, icon-filter-mobile, icon-search, icon-sort-mobile to use currentColor

--- src/shared/ui/icons/index.tsx
+++ src/shared/ui/icons/index.tsx
@@ -1,16 +1,33 @@
 import type { SVGProps } from "react";
+import IconBillDueSVG from "@/src/shared/assets/icons/icon-bill-due.svg";
+import IconBillPaidSVG from "@/src/shared/assets/icons/icon-bill-paid.svg";
 import IconCaretDownSVG from "@/src/shared/assets/icons/icon-caret-down.svg";
 import IconCaretLeftSVG from "@/src/shared/assets/icons/icon-caret-left.svg";
 import IconCaretRightSVG from "@/src/shared/assets/icons/icon-caret-right.svg";
+import IconCloseModalSVG from "@/src/shared/assets/icons/icon-close-modal.svg";
+import IconEllipsisSVG from "@/src/shared/assets/icons/icon-ellipsis.svg";
 import IconFilterMobileSVG from "@/src/shared/assets/icons/icon-filter-mobile.svg";
+import IconHidePasswordSVG from "@/src/shared/assets/icons/icon-hide-password.svg";
 import IconMinimizeMenuSVG from "@/src/shared/assets/icons/icon-minimize-menu.svg";
 import IconNavBudgetsSVG from "@/src/shared/assets/icons/icon-nav-budgets.svg";
 import IconNavOverviewSVG from "@/src/shared/assets/icons/icon-nav-overview.svg";
 import IconNavPotsSVG from "@/src/shared/assets/icons/icon-nav-pots.svg";
 import IconNavRecurringBillsSVG from "@/src/shared/assets/icons/icon-nav-recurring-bills.svg";
 import IconNavTransactionsSVG from "@/src/shared/assets/icons/icon-nav-transactions.svg";
+import IconPotSVG from "@/src/shared/assets/icons/icon-pot.svg";
+import IconRecurringBillsSVG from "@/src/shared/assets/icons/icon-recurring-bills.svg";
 import IconSearchSVG from "@/src/shared/assets/icons/icon-search.svg";
+import IconSelectedSVG from "@/src/shared/assets/icons/icon-selected.svg";
+import IconShowPasswordSVG from "@/src/shared/assets/icons/icon-show-password.svg";
 import IconSortMobileSVG from "@/src/shared/assets/icons/icon-sort-mobile.svg";
+
+export const IconBillDue = (props: SVGProps<SVGSVGElement>) => (
+  <IconBillDueSVG {...props} />
+);
+
+export const IconBillPaid = (props: SVGProps<SVGSVGElement>) => (
+  <IconBillPaidSVG {...props} />
+);
 
 export const IconCaretDown = (props: SVGProps<SVGSVGElement>) => (
   <IconCaretDownSVG {...props} />
@@ -24,8 +41,20 @@ export const IconCaretRight = (props: SVGProps<SVGSVGElement>) => (
   <IconCaretRightSVG {...props} />
 );
 
+export const IconCloseModal = (props: SVGProps<SVGSVGElement>) => (
+  <IconCloseModalSVG {...props} />
+);
+
+export const IconEllipsis = (props: SVGProps<SVGSVGElement>) => (
+  <IconEllipsisSVG {...props} />
+);
+
 export const IconFilterMobile = (props: SVGProps<SVGSVGElement>) => (
   <IconFilterMobileSVG {...props} />
+);
+
+export const IconHidePassword = (props: SVGProps<SVGSVGElement>) => (
+  <IconHidePasswordSVG {...props} />
 );
 
 export const IconMinimizeMenu = (props: SVGProps<SVGSVGElement>) => (
@@ -52,8 +81,24 @@ export const IconNavTransactions = (props: SVGProps<SVGSVGElement>) => (
   <IconNavTransactionsSVG {...props} />
 );
 
+export const IconPot = (props: SVGProps<SVGSVGElement>) => (
+  <IconPotSVG {...props} />
+);
+
+export const IconRecurringBills = (props: SVGProps<SVGSVGElement>) => (
+  <IconRecurringBillsSVG {...props} />
+);
+
 export const IconSearch = (props: SVGProps<SVGSVGElement>) => (
   <IconSearchSVG {...props} />
+);
+
+export const IconSelected = (props: SVGProps<SVGSVGElement>) => (
+  <IconSelectedSVG {...props} />
+);
+
+export const IconShowPassword = (props: SVGProps<SVGSVGElement>) => (
+  <IconShowPasswordSVG {...props} />
 );
 
 export const IconSortMobile = (props: SVGProps<SVGSVGElement>) => (
